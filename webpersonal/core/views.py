79da749536a4c76bd85d46fec5a18a23fe512e1a
Nacho <isanccal@myuax.com>
2024-11-15 12:05:09 +0100

    a

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home(request):
- return HttpResponse("<h1>Mi Web Personal</h1><h2>Esto es la portada</h2>")
+ html_response = "<h1>Mi Web Personal</h1>"
+ for i in range(10):
+    html_response += "<p>Esto es la portada</p>"
+ return HttpResponse(html_response)
