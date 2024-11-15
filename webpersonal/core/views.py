9637a85b233f76896589fdd0ec3b15d0b43df8cf
Nacho <isanccal@myuax.com>
2024-11-15 16:40:09 +0100

    portfolio

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -21,6 +21,15 @@ def about(request):
         <p>Me llamo Nacho y me encanta Django!</p>
  """)
 
+def portfolio(request):
+    return HttpResponse(html_base + """
+        <h2>Portfolio</h2>
+        <p>Algunos de mis proyectos:</p>
+        <ul>
+            <li><a href="https://github.com/DeNexus-Project/DeNexus_CSV-Analysis">DeNexus CSV Analysis</a></li>
+         </ul>
+    """)
+   
 def contact(request):
     return HttpResponse(html_base + """
         <h2>Contacto</h2>
