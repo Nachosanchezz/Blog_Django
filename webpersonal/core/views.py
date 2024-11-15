50de48e28ad59b1268bf36409570c061a2f990fc
Nacho <isanccal@myuax.com>
2024-11-15 12:13:12 +0100

    a

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home(request):
- html_response = "<h1>Mi Web Personal</h1>"
- for i in range(10):
-    html_response += "<p>Esto es la portada</p>"
- return HttpResponse(html_response)
+ return HttpResponse("<h1>Mi Web Personal</h1><h2>Esto es la portada</h2>")
+
+def about(request):
+    return HttpResponse("""
+        <h1>Mi Web Personal</h1>
+        <h2>Acerca de</h2>
+        <p>Me llamo Nacho y me encanta Django!</p>
+ """)
