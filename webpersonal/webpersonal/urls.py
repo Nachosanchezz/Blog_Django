fdb654fd7dc6e2b82719c3deb8a45f1d747991b6
Nacho <isanccal@myuax.com>
2024-11-18 18:47:18 +0100

    camu

--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core import views
+from django.conf import settings 
 
 
 urlpatterns = [
@@ -29,4 +30,7 @@ urlpatterns = [
 
 ] 
 
-
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT)
