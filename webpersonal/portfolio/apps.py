15fa30be716dda1917a9df3d4110fda4c5b3fa2c
Nacho <isanccal@myuax.com>
2024-11-18 18:13:00 +0100

    cambios

--- webpersonal/portfolio/apps.py
+++ webpersonal/portfolio/apps.py
@@ -5,3 +5,5 @@ class PortfolioConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "portfolio"
     verbose_name = "Portfolio"
+
+    
--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
- title = models.CharField(max_length=200)
- description = models.TextField()
- image = models.ImageField()
- created = models.DateTimeField(auto_now_add=True)
- updated = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    class Meta:
+        verbose_name = "proyecto"
+        verbose_name_plural = "proyectos"
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.title
