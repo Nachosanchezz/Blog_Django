d75255ef4e1e8a427e41753efc1a1a38e8efab4f
Nacho <isanccal@myuax.com>
2024-11-21 23:09:29 +0100

    commit

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = "django-insecure-i(6=p1=o%@s$w$7)y%b*h8p#c_b7ht2+(l_sx)m&17sp!9pr%^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blog-django-qqkm.vercel.app', '127.0.0.1', 'localhost']
+
 
 
 # Application definition
